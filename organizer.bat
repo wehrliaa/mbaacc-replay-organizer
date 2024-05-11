2a243eb4daaabd5a0a66fcf5235f5a39e09ce1f5
wehrlia <>
2024-05-11 19:44:53 -0300

    organizer.bat: forgot to remove perl stuff

--- organizer.bat
+++ organizer.bat
@@ -1,13 +1,5 @@
 @echo off
 
-if exist rep-organizer.pl (
-	perl rep-organizer.pl
-	pause
-	exit 0
-)
-
-if exist rep-organizer.py (
-	python3 rep-organizer.py
-	pause
-	exit 0
-)
+python3 rep-organizer.py
+pause
+exit 0
